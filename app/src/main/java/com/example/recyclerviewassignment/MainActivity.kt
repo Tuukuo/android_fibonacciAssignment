99a27ca09abe572837c963f550b0a891f51b9b7c
Student <tuukuodorcas@gmail.com>
2024-06-20 17:23:12 +0300

    add changes

---
+++ app/src/main/java/com/example/recyclerviewassignment/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.recyclerviewassignment
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
